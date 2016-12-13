17b455e3886e94b7f94d70d77a64ff1c5ba5a04e
orlo wang <ow.cc@outlook.com>
2016-12-13 15:55:27 +0800

    add some addons

---
+++ .storybook/addons.js
@@ -0,0 +1,5 @@
+import '@kadira/storybook/addons';
+import '@kadira/storybook-addon-actions/register';
+import '@kadira/storybook-addon-knobs/register'
+import '@kadira/storybook-addon-options/register';
+import 'storybook-readme/register';
