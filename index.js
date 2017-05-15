1e4f32049355d204b01ae7e2186d4f059355a2c5
orlo wang <ow.cc@outlook.com>
2017-05-15 19:17:24 +0800

    reset index.js

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 'use strict';
-var MidfyComponent = {
-  Banner: require('./lib/basic.Banner'),
-  Input: require('./lib/basic.Input'),
-  Scroller: require('./lib/business.Scroller'),
-}
-module.exports = MidfyComponent
+module.exports = {}
